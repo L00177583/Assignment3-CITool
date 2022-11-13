408bee6b86146670a26a68cd715c0593f1d9014d
L00177583 <L00177583@atu.ie>
2022-11-13 20:47:25 +0000

    This is First commit for project CI tool

---
+++ mvn4CI/src/main/java/ie/atu/mvn4CI/App.java
@@ -0,0 +1,13 @@
+package ie.atu.mvn4CI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
