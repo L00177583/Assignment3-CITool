f98edc6b946abcfc0bb3728ed02f0fb9e3dc3b5f
L00177583 <L00177583@atu.ie>
2022-11-14 16:16:04 +0000

    updated the Test outcome

--- mvn4CI/src/test/java/ie/atu/mvn4CI/AppTest.java
+++ mvn4CI/src/test/java/ie/atu/mvn4CI/AppTest.java
@@ -16,6 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("This test is succesful");
+        System.out.println("This test is succesfully updated");
     }
 }
