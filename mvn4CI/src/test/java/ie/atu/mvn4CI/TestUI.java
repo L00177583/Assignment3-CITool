9488946f85e4dcd9ed11cb573a0410e6f234147b
L00177583 <L00177583@atu.ie>
2022-11-18 16:07:25 +0000

    Updated chrome driver setup in UI test

--- mvn4CI/src/test/java/ie/atu/mvn4CI/TestUI.java
+++ mvn4CI/src/test/java/ie/atu/mvn4CI/TestUI.java
@@ -4,6 +4,8 @@ package ie.atu.mvn4CI;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.Assert.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,11 +18,14 @@ public class TestUI
 	
 	{
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		ChromeOptions ChromeOptions = new ChromeOptions();
+		ChromeOptions.addArguments("--headless","--no-sandbox");
+		WebDriver driver = new ChromeDriver(ChromeOptions);
 		driver.manage().window().fullscreen();
 		driver.get("https://www.atu.ie/");
 		System.out.println("Test done");
-		driver.quit();
+//		driver.quit();
 		
 	}
 	
