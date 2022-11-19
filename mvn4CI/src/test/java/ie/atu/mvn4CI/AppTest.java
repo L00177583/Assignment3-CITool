1c84c88583a19a4652e204e63f9546eddbf24f53
Monish-L00177583 <113636371+L00177583@users.noreply.github.com>
2022-11-19 22:55:54 +0000

    Update AppTest.java

--- mvn4CI/src/test/java/ie/atu/mvn4CI/AppTest.java
+++ mvn4CI/src/test/java/ie/atu/mvn4CI/AppTest.java
@@ -16,6 +16,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("This test is succesfully updated");
+        System.out.println("This test is succesfully updated123");
     }
 }
